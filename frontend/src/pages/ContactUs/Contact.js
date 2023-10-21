6cd31dba651a29e99ddab9f20df9deaa9a223210
ramez alrejjal <alrijjalramez@gmail.com>
2023-10-22 00:41:50 +0300

    ramez

--- frontend/src/pages/ContactUs/Contact.js
+++ frontend/src/pages/ContactUs/Contact.js
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
-import sal from 'sal.js';export default function Contact() {
+import sal from 'sal.js';export default function Contact()
+ {
     useEffect(() => {
         sal();
     }, []);
@@ -40,8 +41,11 @@ import sal from 'sal.js';export default function Contact() {
           <div className="section-title mb--30 text-center">
             <h2 className="title">Quick Contact Address</h2>
             <p className="description">
-              There are many variations of passages of Lorem Ipsum available,{" "}
-              <br /> but the majority have suffered alteration.
+            Our goal is to provide an additional enjoyable experience.
+             We encourage you to explore offers and bids, and participate in tenders. 
+             We have a 24/7 support team and an outstanding experience.{" "}
+              <br /> 
+              
             </p>
           </div>
         </div>
@@ -74,10 +78,10 @@ import sal from 'sal.js';export default function Contact() {
             <div className="inner">
               <h4 className="title">Contact Phone Number</h4>
               <p>
-                <a href="tel:+444555666777">+444 555 666 777</a>
+                <a href="tel:+444555666777">00962777777777</a>
               </p>
               <p>
-                <a href="tel:+222222222333">+222 222 222 333</a>
+                <a href="tel:+222222222333">00962799999999</a>
               </p>
             </div>
           </div>
@@ -109,10 +113,10 @@ import sal from 'sal.js';export default function Contact() {
             <div className="inner">
               <h4 className="title">Our Email Address</h4>
               <p>
-                <a href="mailto:admin@gmail.com">admin@gmail.com</a>
+                <a href="mailto:admin@gmail.com">nuron@gmail.com</a>
               </p>
               <p>
-                <a href="mailto:example@gmail.com">example@gmail.com</a>
+                <a href="mailto:example@gmail.com">nuron@gmail.com</a>
               </p>
             </div>
           </div>
@@ -144,7 +148,7 @@ import sal from 'sal.js';export default function Contact() {
             <div className="inner">
               <h4 className="title">Our Location</h4>
               <p>
-                5678 Bangla Main Road, cities 580 <br /> GBnagla, example 54786
+                Jordan , Irbid  <br /> Down Town Bld 55
               </p>
             </div>
           </div>
@@ -194,12 +198,12 @@ import sal from 'sal.js';export default function Contact() {
                 </label>
                 <input id="contact-email" name="contact-email" type="email" />
               </div>
-              <div className="mb-5">
+              {/* <div className="mb-5">
                 <label htmlFor="subject" className="form-label">
                   Subject
                 </label>
                 <input id="subject" name="subject" type="text" />
-              </div>
+              </div> */}
               <div className="mb-5">
                 <label htmlFor="contact-message" className="form-label">
                   Write Message
@@ -241,7 +245,7 @@ import sal from 'sal.js';export default function Contact() {
           data-sal-duration={800}
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235495.62780269101!2d-73.932551722484!3d41.33466214858558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e1!3m2!1sen!2sbd!4v1637254792714!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1077045.414582196!2d35.3177625479389!3d31.182899955931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502f7d72ed883d1%3A0xb2d28ef2eb6f3a91!2sJordan!5e1!3m2!1sen!2sbd!4v1637254792714!5m2!1sen!2sbd"
             height={550}
             style={{ border: 0 }}
             allowFullScreen=""
